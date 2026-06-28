54221d2d28e549ed248f6c71f40ac50cc955c321
mahdimonir <mahdimoniruzzaman@gmail.com>
2026-06-28 11:56:05 +0600

    feat(backend): updated env config

--- backend/config/env.go
+++ backend/config/env.go
@@ -33,16 +33,60 @@ func LoadConfig() *Config {
 		port = "8080"
 	}
 
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = os.Getenv("POSTGRES_URL")
+	}
+	if databaseURL == "" {
+		databaseURL = os.Getenv("POSTGRES_URL_NON_POOLING")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = os.Getenv("POSTGRES_HOST")
+	}
+
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = os.Getenv("POSTGRES_PORT")
+		if dbPort == "" {
+			dbPort = "5432"
+		}
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = os.Getenv("POSTGRES_USER")
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = os.Getenv("POSTGRES_PASSWORD")
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = os.Getenv("POSTGRES_DATABASE")
+	}
+
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+	if dbSSLMode == "" {
+		dbSSLMode = os.Getenv("POSTGRES_SSLMODE")
+		if dbSSLMode == "" {
+			dbSSLMode = "require"
+		}
+	}
+
 	return &Config{
 		Port: port,
 
-		DatabaseURL: os.Getenv("DATABASE_URL"),
-		DBHost:      os.Getenv("DB_HOST"),
-		DBPort:      os.Getenv("DB_PORT"),
-		DBUser:      os.Getenv("DB_USER"),
-		DBPassword:  os.Getenv("DB_PASSWORD"),
-		DBName:      os.Getenv("DB_NAME"),
-		DBSSLMode:   os.Getenv("DB_SSLMODE"),
+		DatabaseURL: databaseURL,
+		DBHost:      dbHost,
+		DBPort:      dbPort,
+		DBUser:      dbUser,
+		DBPassword:  dbPassword,
+		DBName:      dbName,
+		DBSSLMode:   dbSSLMode,
 
 		JWTSecret: os.Getenv("JWT_SECRET"),
 		JWTExpire: os.Getenv("JWT_EXPIRE"),
